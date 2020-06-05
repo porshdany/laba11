0ab65e3423e7fb5de18e79d49da02397c1b48253
porshdany <porsh-porsh@inbox.ru>
2020-06-05 15:02:05 +0300

    first

---
+++ cmake-build-debug/CMakeFiles/builder.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/builder.dir/sources/builder.cpp.o"
+  "libbuilderd.a"
+  "libbuilderd.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/builder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/builder.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libbuilderd.a"
+)
